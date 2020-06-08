05f8b0397b7b625e4deff47ea6112f101bd29af5
nahoko6 <66640733+nahoko6@users.noreply.github.com>
2020-06-08 12:05:56 -0700

    Add files via upload

---
+++ animal_habitat.py
@@ -0,0 +1,31 @@
+camel = "Camel"
+lion = "Lion"
+deer = "Deer"
+goose = "Goose"
+bat = "Bat"
+rabbit = "Rabbit"
+number = ()
+while number == "0" or "1" or "2" or "3" or "4" or "5" or "exit":
+    number = input("What habitat do you want?: ")
+    if number == "0":
+        print(camel)
+    elif number == "1":
+        print(lion)
+    elif number == "2":
+        print(deer)
+    elif number == "3":
+        print(goose)
+    elif number == "4":
+        print(bat)
+    elif number == "5":
+        print(rabbit)
+    elif number == "exit":
+        print("See you!")
+        break
+
+
+
+
+
+
+
